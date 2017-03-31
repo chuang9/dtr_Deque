67d11194681aea3f79e5a617d69db4608d1c1aac
clivejj <cjohnston1@stuy.edu>
2017-03-31 11:36:42 -0400

    Add files via upload
    interface

---
+++ Deque.java
@@ -0,0 +1,19 @@
+//Team dtr-deque(Clive Johnston, Chester Huang, Shaikat Islam)
+
+
+public interface Deque<T> {
+
+    //inserts element in front of deque if possible w/o violating capacity
+    void addFirst(T x);
+    //returns and removes first element of deque; throws exception if deque is empty
+    T removeFirst();
+    //returns first element of deque
+    T getFirst();
+    //inserts element into end of deque if possible w/o violating capacity restrictions
+    void addLast(T x);
+    //returns and removes last element of deque; throws exception if deque is empty
+    T removeLast();
+    //returns last element of deque; throws exception if deque is empty
+    T getLast();
+
+}
